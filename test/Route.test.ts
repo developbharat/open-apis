97bb6cdfe103f17cce24084279b5f5762b29eeeb
Jayant Malik <dev.jayantmalik@gmail.com>
2024-06-18 19:31:43 +0530

    added few test cases for route module

--- test/Route.test.ts
+++ test/Route.test.ts
@@ -1,5 +1,6 @@
-import { describe, it, expect } from 'bun:test'
+import { describe, it, expect, mock } from 'bun:test'
 import { RouteBuilder, Route } from '../src/Route'
+import * as t from '@sinclair/typebox';
 
 describe('Route', () => {
   it('initializes', () => {
@@ -16,4 +17,187 @@ describe('Route', () => {
     expect(created.method).toBe("get");
     expect(created.__handle).toBeFunction();
   })
+
+  it("create POST /article route", () => {
+    const created = Route.setPath("post", "/articles")
+      .setHandle(() => { })
+      .build();
+
+    expect(created.path).toBe("/articles");
+    expect(created.method).toBe("post");
+    expect(created.__handle).toBeFunction();
+  })
+
+  it("create PUT /article route", () => {
+    const created = Route.setPath("put", "/articles")
+      .setHandle(() => { })
+      .build();
+
+    expect(created.path).toBe("/articles");
+    expect(created.method).toBe("put");
+    expect(created.__handle).toBeFunction();
+  })
+
+  it("create PATCH /article route", () => {
+    const created = Route.setPath("patch", "/articles")
+      .setHandle(() => { })
+      .build();
+
+    expect(created.path).toBe("/articles");
+    expect(created.method).toBe("patch");
+    expect(created.__handle).toBeFunction();
+  })
+
+
+  it("create DELETE /article route", () => {
+    const created = Route.setPath("delete", "/articles")
+      .setHandle(() => { })
+      .build();
+
+    expect(created.path).toBe("/articles");
+    expect(created.method).toBe("delete");
+    expect(created.__handle).toBeFunction();
+  })
+
+  it("supports middlewares for GET /articles", async () => {
+    const middleware1 = mock(async () => { });
+    const middleware2 = mock(async () => { });
+    const created = Route.setPath("get", "/articles")
+      .setMiddlewares(middleware1, middleware2)
+      .setHandle(() => { })
+      .build();
+
+    await created.__handle({} as any, { json: () => null, writableEnded: false } as any);
+
+    expect(created.path).toBe("/articles");
+    expect(created.method).toBe("get");
+    expect(created.__handle).toBeFunction();
+    expect(middleware1).toHaveBeenCalled();
+    expect(middleware2).toHaveBeenCalled();
+  });
+
+  it("supports handle for GET /articles", async () => {
+    const handle = mock(async () => { });
+    const created = Route.setPath("get", "/articles")
+      .setHandle(handle)
+      .build();
+
+    await created.__handle({} as any, { json: () => null, writableEnded: false } as any);
+
+    expect(created.path).toBe("/articles");
+    expect(created.method).toBe("get");
+    expect(created.__handle).toBeFunction();
+    expect(handle).toHaveBeenCalled();
+  })
+
+  it("supports params for /articles/:id", () => {
+    const created = Route.setPath("get", "/articles/:id")
+      .setParams(t.Object({ name: t.String({}) }))
+      .setHandle(() => { })
+      .build();
+
+    expect(created.path).toBe("/articles/:id");
+    expect(created.method).toBe("get");
+    expect(created.__handle).toBeFunction();
+  })
+
+  it("supports params for /articles/:article_id/comments/:comment_id", () => {
+    const created = Route.setPath("get", "/articles/:article_id/comments/:comment_id")
+      .setParams(t.Object({
+        article_id: t.String(),
+        comment_id: t.String(),
+      }))
+      .setHandle(() => { })
+      .build();
+
+    expect(created.path).toBe("/articles/:article_id/comments/:comment_id");
+    expect(created.method).toBe("get");
+    expect(created.__handle).toBeFunction();
+  })
+
+  it("validates params presence from route path /articles/:id", () => {
+    const created = () => Route.setPath("get", "/articles/:id")
+      .setHandle(() => { })
+      .build();
+
+    expect(created).toThrow();
+  })
+
+  it("validates validate method for GET /articles", () => {
+    const created = () => Route.setPath("unknown" as any, "/articles")
+      .setHandle(() => { })
+      .build();
+
+    expect(created).toThrow();
+  })
+
+  it("treats blank path for route as /", () => {
+    const created = Route.setPath("get", "")
+      .setHandle(() => { })
+      .build();
+
+    expect(created.path).toBe("/");
+    expect(created.method).toBe("get");
+    expect(created.__handle).toBeFunction();
+
+  });
+
+  it("validates request data for POST /articles", () => {
+    const created = Route.setPath("post", "/articles")
+      .setRequestData(t.Object({
+        title: t.String(),
+        description: t.String(),
+      }))
+      .setHandle(() => { })
+      .build();
+
+    const response = { json: () => null } as any;
+    const success = () => created.__handle({ body: { title: "abc", description: "abcd" } } as any, response);
+    const failure = () => created.__handle({ body: {} } as any, response);
+
+    expect(created.path).toBe("/articles");
+    expect(created.method).toBe("post");
+    expect(created.__handle).toBeFunction();
+    expect(success).not.toThrowError();
+    expect(failure).toThrowError();
+  });
+
+  it("validates request without data for POST /articles", () => {
+    const created = Route.setPath("post", "/articles")
+      .setHandle(() => { })
+      .build();
+
+    const response = { json: () => null } as any;
+    const success = () => created.__handle({ body: {} } as any, response);
+
+    expect(created.path).toBe("/articles");
+    expect(created.method).toBe("post");
+    expect(created.__handle).toBeFunction();
+    expect(success).not.toThrowError();
+  });
+
+
+  it("disallows to use Route.setRequestData with GET and OPTIONS method", () => {
+    const get = () => Route.setPath("get", "/articles")
+      .setRequestData(t.Object({
+        title: t.String(),
+        description: t.String(),
+      }))
+      .setHandle(() => { })
+      .build();
+
+    const options = () => Route.setPath("options", "/articles")
+      .setRequestData(t.Object({
+        title: t.String(),
+        description: t.String(),
+      }))
+      .setHandle(() => { })
+      .build();
+
+    expect(get).toThrowError();
+    expect(options).toThrowError();
+  })
+
+
 })
+
