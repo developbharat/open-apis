cf7693fff33ad4d4faf91fc5c4a6a4188cd52420
Jayant Malik <dev.jayantmalik@gmail.com>
2024-07-08 23:04:41 +0530

    started working on nodejs adapter and form data handling

--- src/contracts/core.ts
+++ src/contracts/core.ts
@@ -1,8 +1,12 @@
+import { Readable } from "stream";
+
+export type IOpenRequestBody = Record<string, string | number | boolean | IOpenRequestFile>;
+
 export interface OpenRequest {
   readonly params: Record<string, string>;
   readonly query: Record<string, string>;
   readonly headers: Record<string, string>;
-  readonly body: Record<string, any>;
+  readonly body: IOpenRequestBody;
   readonly secure: boolean;
   readonly path: string;
   readonly hostname: string;
@@ -15,4 +19,15 @@ export interface OpenResponse {
   writableEnded: boolean;
   write(data: string): any;
   end(data?: string): any;
+}
+
+export interface IOpenRequestFile {
+  filename: string;
+  tmpFilePath: string;
+  encoding: string;
+  extension: string;
+  mime: string;
+  size: number;
+  stream: Readable;
+  isSavedToDisk: boolean;
 }
